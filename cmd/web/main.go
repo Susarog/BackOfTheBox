436692cab0afaf303276972449ab2cbc79dbc0e7
Ryan Hang <ryanhang92802@gmail.com>
2024-01-13 15:34:47 -0800

    Add Strucutred Logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	var app application
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	app.logger = logger
+
+	logger.Info("starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, routes())
-	if err != nil {
-		log.Println(err.Error())
-	}
+	logger.Error(err.Error())
 }
