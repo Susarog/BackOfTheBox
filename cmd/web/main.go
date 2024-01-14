25a970ee6c43252ab994ee7ef741e96392b555dc
Ryan Hang <ryanhang92802@gmail.com>
2024-01-13 19:33:02 -0800

    Add Database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -13,6 +16,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "admin:mysecretpassword@localhost:5432/postgres", "Postgres data source name")
 	flag.Parse()
 
 	var app application
@@ -20,7 +24,14 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	app.logger = logger
 
+	db, err := sql.Open("postgres", *dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, routes())
+	err = http.ListenAndServe(*addr, routes())
 	logger.Error(err.Error())
 }
